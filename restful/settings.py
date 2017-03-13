bf270efe0515f6ea9c98df51d0cbb6e573225967
rpraval1 <rpraval1@binghamton.edu>
2017-03-13 13:57:19 -0400

    Set path to template drectory under project settings

--- restful/settings.py
+++ restful/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'restful.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['restful/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
