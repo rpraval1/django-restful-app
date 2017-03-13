cb80d47661e8ac39619a0b16a7d08bdfbb14b8ab
rpraval1 <rpraval1@binghamton.edu>
2017-03-13 14:44:05 -0400

    Created Models to let user enter data

--- djangoRestfulApp/admin.py
+++ djangoRestfulApp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import OdmOrganization
+from .models import OdmPeople
+
+class OdmOrganizationAdmin(admin.ModelAdmin):
+    fields = ['name', 'domain_name', 'locations']
+
+class OdmPeopleAdmin(admin.ModelAdmin):
+    fields = ['name', 'locations', 'socials', 'types']
+
+admin.site.register(OdmOrganization, OdmOrganizationAdmin)
+admin.site.register(OdmPeople, OdmPeopleAdmin)
--- djangoRestfulApp/models.py
+++ djangoRestfulApp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class OdmOrganization(models.Model):
+    name = models.CharField(max_length=200)
+    domain_name = models.CharField(max_length=200)
+    locations = models.CharField(max_length=200)
+
+    def __str__(self):
+        return '%s %s %s' % (self.name, self.domain_name, self.locations)
+
+class OdmPeople(models.Model):
+    name = models.CharField(max_length=200)
+    locations = models.CharField(max_length=200)
+    socials = models.CharField(max_length=200)
+    types = models.CharField(max_length=200)
+
+    def __str__(self):
+        return '%s %s %s %s' % (self.name, self.domain_name, self.locations, self.types)
