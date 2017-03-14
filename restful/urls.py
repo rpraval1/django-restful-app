0c6a670ab4c2348b78451709a34e4942d99550b7
rpraval1 <rpraval1@binghamton.edu>
2017-03-14 15:34:42 -0400

    Added css

--- restful/urls.py
+++ restful/urls.py
@@ -23,10 +23,10 @@ router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
-    #url(r'^$', views.index),
+    url(r'^$', views.index),
     url(r'^admin/', admin.site.urls),
     url(r'^organizations$', views.organization),
     url(r'^people$', views.people),
-    url(r'^', include(router.urls)),
+    #url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
